581ab40f338b8bf08b0145f80602e256dd27ac13
Uma Jangam <umajangam97@gmail.com>
2022-01-08 15:03:44 -0600

    TemperatureF is changed

--- TimeTracking-Api/WeatherForecast.cs
+++ TimeTracking-Api/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace TimeTracking_Api
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 22 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
        
         public string? Summary { get; set; }
     }
