936c4f4cb849b29dc199b327206dffd7d8b02075
Uma Jangam <umajangam97@gmail.com>
2022-01-12 18:33:02 -0600

    Enumerable is changed to 6

--- TimeTracking-Api/Controllers/WeatherForecastController.cs
+++ TimeTracking-Api/Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace TimeTracking_Api.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 3).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 6).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
