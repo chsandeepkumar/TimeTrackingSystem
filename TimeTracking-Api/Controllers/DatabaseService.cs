19586fae083f57f308b544bcb2183e7c69b18211
Uma Jangam <umajangam97@gmail.com>
2022-01-23 10:59:18 -0600

    Login,DatabaseService and LoginController is updated.

--- TimeTracking-Api/Controllers/DatabaseService.cs
+++ TimeTracking-Api/Controllers/DatabaseService.cs
@@ -17,7 +17,7 @@ namespace TimeTracking_Api.Controllers
                 sqlConnection.Open();
                 SqlCommand cmd = sqlConnection.CreateCommand();
                 cmd.CommandText = $"Insert into Users(id,created_date_time,email,first_name,last_name,role_id,updated_date_time,name) values " +
-                    $"('{user.Id}','{user.CreatedDateTime}','{user.Email}','{user.FirstName}','{user.LastName}','{user.Roleid}','{user.UpdatedDateTime}','{user.Name}') ";
+                    $"({user.Id},'{user.CreatedDateTime}','{user.Email}','{user.FirstName}','{user.LastName}','{user.Roleid}','{user.UpdatedDateTime}','{user.Name}') ";
                 var result = cmd.ExecuteNonQuery();
                 return result;
             }
@@ -109,7 +109,93 @@ namespace TimeTracking_Api.Controllers
                 throw ex;
             }
         }
+        public int UserLogin(Login login)
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = DatabaseConnectionString;
 
+                sqlConnection.Open();
+                SqlCommand cmd = sqlConnection.CreateCommand();
+                cmd.CommandText = $"Insert into Login(login_id,password,username,is_active) values " +
+                    $"(10,'{login.Password}','{login.UserName}','{login.IsActive}') ";
+                var result = cmd.ExecuteNonQuery();
+                return result;
+            }
+            catch (Exception exception)
+            {
+
+                return -1;
+            }
+
+        }
+        public List<Login> GetLoginUser(int loginid)
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = DatabaseConnectionString;
+                sqlConnection.Open();
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandText = $"select * from Login where login_id={loginid}";
+                SqlDataReader result = sqlCommand.ExecuteReader();
+                List<Login> loginusers = new List<Login>();
+                while (result.Read())
+                {
+                    Login loginuser = new Login();
+                    loginuser.LoginId = result.GetInt32(0);
+                    loginuser.Password = result.GetString(1);
+                    loginuser.UserName = result.GetString(2);
+                    loginuser.IsActive = result.GetBoolean(3);
+                    loginusers.Add(loginuser);
+                }
+                result.Close();
+
+                return loginusers;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public int UpdateLoginUser(Login login)
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = DatabaseConnectionString;
+                sqlConnection.Open();
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+
+                sqlCommand.CommandText = $"update Login set password='{login.Password}',username='{login.UserName}',is_active='{login.IsActive}' where login_id={login.LoginId}";
+
+                var updatedRecordCount = sqlCommand.ExecuteNonQuery();
+                return updatedRecordCount;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public int DeleteLoginUser(int loginid)
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = DatabaseConnectionString;
+                sqlConnection.Open();
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandText = $"Delete from login where login_id='{loginid}'";
+                var var = sqlCommand.ExecuteNonQuery();
+                return var;
+            }
+            catch (Exception exception)
+            {
+                return 0;
+            }
+        }
     }
 
 }
---
+++ TimeTracking-Api/Controllers/LoginController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimeTracking_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult UserLogin(Login login)
+        {
+            try
+            {
+                DatabaseService databaseService = new DatabaseService();
+
+                var numberOfRecordsCreated = databaseService.UserLogin(login);
+
+                return Ok(numberOfRecordsCreated);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        [HttpGet]
+        public IActionResult GetLoginUser(int loginid)
+        {
+            try
+            {
+                DatabaseService databaseService = new DatabaseService();
+
+                var listOfUsers = databaseService.GetLoginUser(loginid);
+
+                return Ok(listOfUsers);
+
+            }
+            catch (Exception exception)
+            {
+
+                throw exception;
+            }
+
+        }
+        [HttpPut]
+        public IActionResult UpdateLoginUser(Login login)
+        {
+            try
+            {
+                DatabaseService databaseService = new DatabaseService();
+                var updatedLoginUser = databaseService.UpdateLoginUser(login);
+                return Ok(updatedLoginUser);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpDelete]
+        public IActionResult DeleteLoginUser(int loginid)
+        {
+            try
+            {
+                DatabaseService databaseService = new DatabaseService();
+                var deletedUser = databaseService.DeleteLoginUser(loginid);
+                return Ok(deletedUser);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
